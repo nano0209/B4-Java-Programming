9bb8ac0b2db77fe01f067cd33b7b2f0881051e7d
naima <naimamus36@gmail.com>
2024-11-29 21:40:46 -0500

    Topic about nested loop1

---
+++ src/day20_nested_loops/ContinueInNested.java
@@ -0,0 +1,31 @@
+package day20_nested_loops;
+
+public class ContinueInNested {
+    public static void main(String[] args) {
+        // OUTER LOOP
+        for (int i = 0; i < 5; i++) {
+
+            if (i == 3) {
+                continue;
+            }
+
+            System.out.println("i: " + i);
+
+
+            // INNER LOOP
+            for (int j = 0; j < 3; j++) {
+
+                if (j == 1) {
+                    continue;
+                }
+
+                System.out.println("j: " + j);
+
+
+            }
+
+        }
+
+
+    }
+}

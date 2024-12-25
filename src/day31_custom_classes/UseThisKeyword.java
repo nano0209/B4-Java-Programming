8733bbb97fb5a95f0139b71a2a49dfe261bffb90
naima <naimamus36@gmail.com>
2024-12-24 22:07:55 -0500

    Constructor classes

---
+++ src/day31_custom_classes/UseThisKeyword.java
@@ -0,0 +1,11 @@
+package day31_custom_classes;
+
+public class UseThisKeyword {
+    public static void main(String[] args) {
+        ThisKeyword obj = new ThisKeyword(100);
+
+        System.out.println(obj.a);
+
+
+    }
+}

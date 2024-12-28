8418cd1253ff6b5df924dc6c79f9cabda4da3d65
naima <naimamus36@gmail.com>
2024-12-27 22:09:40 -0500

    Constructor overloading and constructor chaining

---
+++ src/day32_custom_classes/Employee.java
@@ -0,0 +1,38 @@
+package day32_custom_classes;
+
+public class Employee {
+    // name, id, job title, salary
+    // Instance variable - does not have "static" keyword
+    String name;
+    int id;
+    String jobTitle;
+    double salary;
+
+
+    // Constructor - name and jobTitle
+    public Employee(String jobTitle, String name) {
+        this.name = name;
+        this.jobTitle = jobTitle;
+    }
+
+
+    // Constructor - name, id, job title, and salary
+    public Employee(String name, int id, String jobTitle, double salary) {
+        this.name = name;
+        this.id = id;
+        this.jobTitle = jobTitle;
+        this.salary = salary;
+    }
+
+    // Instance Method - does not have "static" keyword
+    public void goToMeeting() {
+        //  $name is going to a meeting
+        System.out.println(name + " is going to a meeting");
+    }
+
+
+    // Instance Method - does not have "static" keyword
+    public String toString() {
+        return name + " - " + jobTitle + " - " + id + " - $" + salary;
+    }
+}

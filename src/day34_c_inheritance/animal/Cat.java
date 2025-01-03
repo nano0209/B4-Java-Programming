0ef7c727cdf25868d48714a1f6d727e8ae3a3509
naima <naimamus36@gmail.com>
2025-01-02 22:11:39 -0500

    OOP Inheritance

---
+++ src/day34_c_inheritance/animal/Cat.java
@@ -0,0 +1,20 @@
+package day34_c_inheritance.animal;
+//template class
+public class Cat extends Animal{
+
+//    //Instance Variables
+//    String type;
+//    int numOfLegs;
+//
+//    // Instance Method
+//    public void walk(){
+//        System.out.println(type + " is walking");
+//    }
+
+    //Instance method
+    public void meow(){
+        System.out.println(type + " is meowing");
+    }
+
+
+}

2711baadec5be74a7acc5724e9d0b5bc25f8c1c5
naima <naimamus36@gmail.com>
2025-01-29 19:15:52 -0500

    Function, stream methods

---
+++ src/practice/Password.java
@@ -0,0 +1,19 @@
+package practice;
+
+public class Password {
+
+    private String password;
+
+    public void setPassword(String password) {
+        if (password.length() >= 8) {
+            for (int i = 0; i < password.length(); i++) {
+
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+
+    }
+}
